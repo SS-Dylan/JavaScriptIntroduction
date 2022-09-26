346a8c42f5616e280b6e2d99a3e8d2354330314c
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-09-26 12:29:31 -0400

    Initial

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,31 +2,40 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace JavaScriptIntroduction.Controllers
+namespace JavaScriptIntroduction.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult JavaScript1()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult JavaScript2()
+    {
+        return View();
+    }
+    
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ wwwroot/js/myjs.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+let h1s = document.getElementsByTagName("h1");
+h1s[0].style.backgroundColor = "green";
+
+let tax = 0;
+let tax2 = 3.4;
+let $tax3 = 5.6;
+let _tax4 = 7.8;
+console.log(tax2);
+
+let o1 = {};
+let o2 = { name: "John", age: 30, grades: ["A", "B", "C"] };
+console.log(o2);
+console.log(o2.name);
+console.log(o2["name"]);
+console.log(o2.grades[1]);
+o2.address = "101";
